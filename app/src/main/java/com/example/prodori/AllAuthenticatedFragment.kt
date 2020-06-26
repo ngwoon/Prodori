4cb4ee31006cd8c0f52974a18c6f0a9b83fa4ab0
ngwoon <todory2002@gmail.com>
2020-06-26 20:05:14 +0900

    다이얼로그 수정

--- app/src/main/java/com/example/prodori/AllAuthenticatedFragment.kt
+++ app/src/main/java/com/example/prodori/AllAuthenticatedFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.SearchView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,7 +30,7 @@ class AllAuthenticatedFragment : Fragment() {
         val postDatabase = FirebaseDatabase.getInstance().getReference("posts")
         postDatabase.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
-                Toast.makeText(mContext, "게시글 로드 실패", Toast.LENGTH_SHORT).show()
+
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
